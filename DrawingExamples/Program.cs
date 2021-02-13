7814d5e1fb3b1aed5c127c047b1004fe6f9a6e33
Viktor Ponomarenko <v.ponomarenko@traderevolution.com>
2021-02-13 09:52:05 +0200

    add solution

---
+++ DrawingExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DrawingExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
